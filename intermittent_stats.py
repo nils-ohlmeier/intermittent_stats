509068f6273cdfe2d27bdb8180f9fba322e2f741
Nils Ohlmeier <nils@mozilla.com>
2014-05-13 00:36:54 -0700

    added another counter for build slave machines

--- intermittent_stats.py
+++ intermittent_stats.py
@@ -10,13 +10,16 @@ from collections import Counter
 BUGZILLA_URL = "https://bugzilla.mozilla.org/rest.cgi/bug/"
 BUGZILLA_COMMENTS = "/comment"
 EMAIL_TBPL = "tbplbot@gmail.com"
+MAX_SLAVES = 10
 
-def printPrettyCounter(counter, name):
+def printPrettyCounter(counter, name, maxval=None):
   total = sum(counter.values()) + 0.0
   print "%s:" % (name)
-  for c in counter.most_common():
+  for c in counter.most_common(maxval):
     percent = c[1] / total * 100
     print "  %d (%.1f%s):  %s" % (c[1], percent, '%', c[0])
+  if maxval and len(counter.most_common()) > maxval:
+    print "  + %d more" % (total - maxval)
 
 if len(sys.argv) < 2:
   sys.exit('Usage: %s bugzilla-number' % sys.argv[0])
@@ -32,7 +35,10 @@ branch = Counter()
 build = Counter()
 testgrp = Counter()
 date = time = []
-reobj = re.compile(r'^(.*) ([a-z0-9-_]+) ([a-z]+) test ([A-Za-z0-9-]+) on ([0-9-]+) ([0-9:]+)$')
+slaves = Counter()
+
+reline3 = re.compile(r'^(.*) ([a-z0-9-_]+) ([a-z]+) test ([A-Za-z0-9-]+) on ([0-9-]+) ([0-9:]+)$')
+reline5 = re.compile(r'slave: (.*)')
 
 connection = urllib2.urlopen(BUGZILLA_URL + str(bugID) + BUGZILLA_COMMENTS)
 jsonDict = json.loads(connection.read())
@@ -40,9 +46,9 @@ jsonDict = json.loads(connection.read())
 comments = jsonDict['bugs'][str(bugID)]['comments']
 for c in comments:
   if c['author'] == EMAIL_TBPL :
-    # TODO: read the line with the machine name as well?!
-    line = re.split("\r?\n", c['text'])[2]
-    match = reobj.match(line)
+    lines = re.split("\r?\n", c['text'])
+    line3 = lines[2]
+    match = reline3.match(line3)
     if match:
       os[match.group(1)] += 1
       branch[match.group(2)] += 1
@@ -52,9 +58,17 @@ for c in comments:
       date.append(match.group(5))
       time.append(match.group(6))
     else:
-      print "NO MATCH: " + line
+      print "non matching line 3: " + line3
+    line5 = lines[4]
+    match = reline5.match(line5)
+    if match:
+      slaves[match.group(1)] += 1
+    else:
+      print "non matching line 5: " + line5
+
 
 printPrettyCounter(os, "OS's")
 printPrettyCounter(branch, "Branches")
 printPrettyCounter(build, "Build")
 printPrettyCounter(testgrp, "Test Group")
+printPrettyCounter(slaves, "Slaves", MAX_SLAVES)
